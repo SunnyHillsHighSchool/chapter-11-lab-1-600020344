a5ed0a67cae4760415f5d0689a18e79e9eaca7f6
600020344 <77069279+600020344@users.noreply.github.com>
2021-01-26 23:08:54 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 class Main {
   public static void main(String[] args) {
     //add test cases 
+    ReverseNumber a = new ReverseNumber(234);
+    System.out.println(a);
+    a.setNumber(10000);
+    System.out.println(a);
+    a.setNumber(111);
+    System.out.println(a);
+    a.setNumber(9005);
+    System.out.println(a);
+    a.setNumber(84645);
+    System.out.println(a);
+    a.setNumber(8547);
+    System.out.println(a);
+    a.setNumber(123456789);
+    System.out.println(a);
   }
 }
